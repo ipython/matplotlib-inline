44775ba8e1cb3c80ca79778df5c3640b9742b0a8
M Bussonnier <bussonniermatthias@gmail.com>
2024-12-10 12:05:43 +0100

    revsplit

--- matplotlib_inline/__init__.py
+++ matplotlib_inline/__init__.py
@@ -1,4 +1,8 @@
 from . import backend_inline, config  # noqa
 
-version_info = (0, 1, 7)
-__version__ = '.'.join(str(s) for s in version_info)  # noqa
+__version__ = "0.1.7"
+
+# we can't ''.join(...) otherwise finding the version number at build time requires
+# import which introduces IPython and matplotlib at build time, and thus circular
+# dependencies.
+version_info = tuple(int(s) for s in __version__.split(".")[:3])
